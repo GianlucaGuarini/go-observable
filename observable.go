d386f0081a66ac3d2c0a79ff4c734dd3165869c0
Gianluca Guarini <gianluca.guarini@gmail.com>
2018-08-29 22:16:09 +0200

    fixed: indentation

--- observable.go
+++ observable.go
@@ -1,8 +1,8 @@
 package observable
 
 import (
-  "reflect"
-  "sync"
+	"reflect"
+	"sync"
 )
 
 // event key uset to listen and remove all the events
@@ -12,31 +12,31 @@ const ALL_EVENTS_NAMESPACE = "*"
 
 // private struct
 type callback struct {
-  fn        reflect.Value
-  isUnique  bool
-  isTyped   bool
-  wasCalled bool
+	fn        reflect.Value
+	isUnique  bool
+	isTyped   bool
+	wasCalled bool
 }
 
 // Public Observable struct
 type Observable struct {
-  Callbacks map[string][]callback
-  *sync.RWMutex
+	Callbacks map[string][]callback
+	*sync.RWMutex
 }
 
 // Public API
 
 // New - returns a new observable reference
 func New() *Observable {
-  return &Observable{
-    make(map[string][]callback),
-    &sync.RWMutex{},
-  }
+	return &Observable{
+		make(map[string][]callback),
+		&sync.RWMutex{},
+	}
 }
 
 // On - adds a callback function
 func (o *Observable) On(event string, cb interface{}) *Observable {
-  return o.addCallback(event, cb, false)
+	return o.addCallback(event, cb, false)
 }
 
 // Trigger - a particular event passing custom arguments
@@ -44,22 +44,22 @@ func (o *Observable) Trigger(event string, params ...interface{}) *Observable {
 	o.Lock()
 	defer o.Unlock()
 
-  // get the arguments we want to pass to our listeners callbaks
-  arguments := make([]reflect.Value, len(params))
+	// get the arguments we want to pass to our listeners callbaks
+	arguments := make([]reflect.Value, len(params))
 
-  // get all the arguments
-  for i, param := range params {
-    arguments[i] = reflect.ValueOf(param)
-  }
+	// get all the arguments
+	for i, param := range params {
+		arguments[i] = reflect.ValueOf(param)
+	}
 
-  o.dispatchEvent(event, arguments)
+	o.dispatchEvent(event, arguments)
 
-  // trigger the all events callback whenever this event was defined
-  if o.hasEvent(ALL_EVENTS_NAMESPACE) && event != ALL_EVENTS_NAMESPACE {
-    o.dispatchEvent(ALL_EVENTS_NAMESPACE, append([]reflect.Value{reflect.ValueOf(event)}, arguments...))
-  }
+	// trigger the all events callback whenever this event was defined
+	if o.hasEvent(ALL_EVENTS_NAMESPACE) && event != ALL_EVENTS_NAMESPACE {
+		o.dispatchEvent(ALL_EVENTS_NAMESPACE, append([]reflect.Value{reflect.ValueOf(event)}, arguments...))
+	}
 
-  return o
+	return o
 }
 
 // Off - stop listening a particular event
@@ -71,21 +71,21 @@ func (o *Observable) Off(event string, args ...interface{}) *Observable {
 
 /// Ditto, necessary for internal calls
 func (o *Observable) offNoSync(event string, args ...interface{}) *Observable {
-  if len(args) == 0 {
-    // wipe all the event listeners
-    if event == ALL_EVENTS_NAMESPACE {
-      o.Callbacks = make(map[string][]callback)
-    }
-  } else if len(args) == 1 {
-    o.removeEvent(event, args[0])
-  } else {
-    panic("Multiple off callbacks are not supported")
-  }
+	if len(args) == 0 {
+		// wipe all the event listeners
+		if event == ALL_EVENTS_NAMESPACE {
+			o.Callbacks = make(map[string][]callback)
+		}
+	} else if len(args) == 1 {
+		o.removeEvent(event, args[0])
+	} else {
+		panic("Multiple off callbacks are not supported")
+	}
 
-  return o
+	return o
 }
 
 // One - call the callback only once
 func (o *Observable) One(event string, cb interface{}) *Observable {
-  return o.addCallback(event, cb, true)
+	return o.addCallback(event, cb, true)
 }
--- observable_test.go
+++ observable_test.go
@@ -1,218 +1,218 @@
 package observable_test
 
 import (
-  "github.com/GianlucaGuarini/go-observable"
-  "sync"
-  "testing"
-  "time"
+	"github.com/GianlucaGuarini/go-observable"
+	"sync"
+	"testing"
+	"time"
 )
 
 func TestOn(t *testing.T) {
 
-  o := observable.New()
-  n := 0
+	o := observable.New()
+	n := 0
 
-  o.On("foo", func() {
-    n++
-  }).On("bar", func() {
-    n++
-  }).On("foo", func() {
-    n++
-  })
+	o.On("foo", func() {
+		n++
+	}).On("bar", func() {
+		n++
+	}).On("foo", func() {
+		n++
+	})
 
-  o.Trigger("foo").Trigger("foo").Trigger("bar")
+	o.Trigger("foo").Trigger("foo").Trigger("bar")
 
-  if n != 5 {
-    t.Errorf("The counter is %d instead of being %d", n, 5)
-  }
+	if n != 5 {
+		t.Errorf("The counter is %d instead of being %d", n, 5)
+	}
 
 }
 
 func TestOnTriggerMultipleEvensString(t *testing.T) {
-  o := observable.New()
-  n := 0
+	o := observable.New()
+	n := 0
 
-  var lastEvt string
+	var lastEvt string
 
-  o.On("foo bar", func(eventName string) {
-    lastEvt = eventName
-    n++
-  })
+	o.On("foo bar", func(eventName string) {
+		lastEvt = eventName
+		n++
+	})
 
-  o.Trigger("foo bar").Trigger("bar foo")
+	o.Trigger("foo bar").Trigger("bar foo")
 
-  if lastEvt != "foo" {
-    t.Errorf("The last event name triggered is %s instead of being %s", lastEvt, "foo")
-  }
+	if lastEvt != "foo" {
+		t.Errorf("The last event name triggered is %s instead of being %s", lastEvt, "foo")
+	}
 
-  if n != 4 {
-    t.Errorf("The counter is %d instead of being %d", n, 4)
-  }
+	if n != 4 {
+		t.Errorf("The counter is %d instead of being %d", n, 4)
+	}
 }
 
 func TestOffMultipleEvensString(t *testing.T) {
-  o := observable.New()
-  n := 0
+	o := observable.New()
+	n := 0
 
 	increment := func(args ...interface{}) {
 		n++
 	}
 
-  o.On("foo bar", increment).On("baz", increment)
+	o.On("foo bar", increment).On("baz", increment)
 
-  o.Off("foo bar baz", increment)
+	o.Off("foo bar baz", increment)
 
-  o.Trigger("foo bar baz")
+	o.Trigger("foo bar baz")
 
-  if n != 0 {
-    t.Errorf("The counter is %d instead of being %d", n, 0)
-  }
+	if n != 0 {
+		t.Errorf("The counter is %d instead of being %d", n, 0)
+	}
 
 }
 
 func TestOnAll(t *testing.T) {
-  o := observable.New()
-  n := 0
-  var lastEvt string
+	o := observable.New()
+	n := 0
+	var lastEvt string
 
-  onAll := func(eventName string) {
-    lastEvt = eventName
-    n++
-  }
+	onAll := func(eventName string) {
+		lastEvt = eventName
+		n++
+	}
 
-  o.On("*", onAll)
+	o.On("*", onAll)
 
-  o.Trigger("foo bar").Trigger("foo").Trigger("bar")
+	o.Trigger("foo bar").Trigger("foo").Trigger("bar")
 
-  o.Off("*", onAll)
+	o.Off("*", onAll)
 
-  o.Trigger("foo bar").Trigger("foo")
+	o.Trigger("foo bar").Trigger("foo")
 
-  if lastEvt != "bar" {
-    t.Errorf("The last event name triggered is %s instead of being %s", lastEvt, "bar")
-  }
+	if lastEvt != "bar" {
+		t.Errorf("The last event name triggered is %s instead of being %s", lastEvt, "bar")
+	}
 
-  if n != 3 {
-    t.Errorf("The counter is %d instead of being %d", n, 3)
-  }
+	if n != 3 {
+		t.Errorf("The counter is %d instead of being %d", n, 3)
+	}
 
 }
 
 func TestOffAll(t *testing.T) {
-  o := observable.New()
-  n := 0
+	o := observable.New()
+	n := 0
 
-  o.On("foo", func() {
-    n++
-  })
+	o.On("foo", func() {
+		n++
+	})
 
-  o.On("bar", func() {
-    n++
-  })
+	o.On("bar", func() {
+		n++
+	})
 
-  o.Off("*")
+	o.Off("*")
 
-  o.Trigger("foo").Trigger("bar").Trigger("foo bar")
+	o.Trigger("foo").Trigger("bar").Trigger("foo bar")
 
-  if n != 0 {
-    t.Errorf("The counter is %d instead of being %d", n, 0)
-  }
+	if n != 0 {
+		t.Errorf("The counter is %d instead of being %d", n, 0)
+	}
 }
 
 func TestOff(t *testing.T) {
-  o := observable.New()
-  n := 0
+	o := observable.New()
+	n := 0
 
-  onFoo1 := func() {
-    n++
-  }
+	onFoo1 := func() {
+		n++
+	}
 
-  onFoo2 := func() {
-    n++
-  }
+	onFoo2 := func() {
+		n++
+	}
 
-  o.On("foo", onFoo1).On("foo", onFoo2)
+	o.On("foo", onFoo1).On("foo", onFoo2)
 
-  o.Off("foo", onFoo1).Off("foo", onFoo2).On("foo", onFoo1)
+	o.Off("foo", onFoo1).Off("foo", onFoo2).On("foo", onFoo1)
 
-  o.Trigger("foo")
+	o.Trigger("foo")
 
-  if n != 1 {
-    t.Errorf("The counter is %d instead of being %d", n, 1)
-  }
+	if n != 1 {
+		t.Errorf("The counter is %d instead of being %d", n, 1)
+	}
 
 }
 
 func TestRace(t *testing.T) {
 
-  o := observable.New()
-  n := 0
+	o := observable.New()
+	n := 0
 
-  asyncTask := func(wg *sync.WaitGroup) {
-    o.Trigger("foo")
-    wg.Done()
-  }
-  var wg sync.WaitGroup
+	asyncTask := func(wg *sync.WaitGroup) {
+		o.Trigger("foo")
+		wg.Done()
+	}
+	var wg sync.WaitGroup
 
-  wg.Add(5)
+	wg.Add(5)
 
-  o.On("foo", func() {
-    n++
-  })
+	o.On("foo", func() {
+		n++
+	})
 
-  go asyncTask(&wg)
-  go asyncTask(&wg)
-  go asyncTask(&wg)
-  go asyncTask(&wg)
-  go asyncTask(&wg)
+	go asyncTask(&wg)
+	go asyncTask(&wg)
+	go asyncTask(&wg)
+	go asyncTask(&wg)
+	go asyncTask(&wg)
 
-  wg.Wait()
+	wg.Wait()
 
-  if n != 5 {
-    t.Errorf("The counter is %d instead of being %d", n, 5)
-  }
+	if n != 5 {
+		t.Errorf("The counter is %d instead of being %d", n, 5)
+	}
 
 }
 
 func TestOne(t *testing.T) {
-  o := observable.New()
-  n := 0
+	o := observable.New()
+	n := 0
 
-  onFoo := func() {
-    n++
-  }
+	onFoo := func() {
+		n++
+	}
 
-  o.One("foo", onFoo)
+	o.One("foo", onFoo)
 
-  o.Trigger("foo").Trigger("foo").Trigger("foo")
+	o.Trigger("foo").Trigger("foo").Trigger("foo")
 
-  if n != 1 {
-    t.Errorf("The counter is %d instead of being %d", n, 1)
-  }
+	if n != 1 {
+		t.Errorf("The counter is %d instead of being %d", n, 1)
+	}
 
 }
 
 func TestArguments(t *testing.T) {
-  o := observable.New()
-  n := 0
-  o.On("foo", func(arg1 bool, arg2 string) {
-    n++
-    if arg1 != true || arg2 != "bar" {
-      t.Error("The arguments must be correctly passed to the callback")
-    }
-  })
+	o := observable.New()
+	n := 0
+	o.On("foo", func(arg1 bool, arg2 string) {
+		n++
+		if arg1 != true || arg2 != "bar" {
+			t.Error("The arguments must be correctly passed to the callback")
+		}
+	})
 
-  o.Trigger("foo", true, "bar")
+	o.Trigger("foo", true, "bar")
 
-  if n != 1 {
-    t.Errorf("The counter is %d instead of being %d", n, 1)
-  }
+	if n != 1 {
+		t.Errorf("The counter is %d instead of being %d", n, 1)
+	}
 }
 
 func TestTrigger(t *testing.T) {
-  o := observable.New()
-  // the trigger without any listener should not throw errors
-  o.Trigger("foo")
+	o := observable.New()
+	// the trigger without any listener should not throw errors
+	o.Trigger("foo")
 }
 
 /**
@@ -222,20 +222,20 @@ func TestTrigger(t *testing.T) {
 var eventsList = []string{"foo", "bar", "baz", "boo"}
 
 func BenchmarkOnTrigger(b *testing.B) {
-  o := observable.New()
-  n := 0
+	o := observable.New()
+	n := 0
 
-  for _, e := range eventsList {
-    o.On(e, func() {
-      n++
-    })
-  }
+	for _, e := range eventsList {
+		o.On(e, func() {
+			n++
+		})
+	}
 
-  for i := 0; i < b.N; i++ {
-    for _, e := range eventsList {
-      o.Trigger(e)
-    }
-  }
+	for i := 0; i < b.N; i++ {
+		for _, e := range eventsList {
+			o.Trigger(e)
+		}
+	}
 }
 
 /**
